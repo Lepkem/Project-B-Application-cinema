6110183dec6d8aeea3714891070aceb3866e60fa
trevorfly <trevorfly123@gmail.com>
2020-03-30 16:02:23 +0200

    added case structure to program, added login function

--- Cinema/Cinema/Program.cs
+++ Cinema/Cinema/Program.cs
@@ -9,13 +9,53 @@ namespace Cinema
     {
         static public List<Room> rooms = new List<Room>();
         static public List<ScheduleElement> schedule = new List<ScheduleElement>();
+        
         static void Main(string[] args)
         {
-            readRooms();
-            //Test update room
-            //rooms[2].updateRoom(@".\rooms\room3.json");
-            //Test create room
-            //createRoom();
+            //console program
+            Boolean running = true;
+            int caseSwitch = 0;
+            Boolean login = false;
+            while(running){
+                switch (caseSwitch)
+                {   //functions
+                    case 0:
+                        //menu
+                        caseSwitch = Menu(login);
+                        break;
+
+                    case 1:
+                        //Login
+                        if (!login){ login = Login(); }
+                        else { Console.WriteLine("\n\n"); login = false; }
+                        caseSwitch = 0;
+                        break;
+
+                    case 2:
+                        //read rooms
+                        readRooms();
+                        //Test update room
+                        //rooms[2].updateRoom(@".\rooms\room3.json");
+                        //Test create room
+                        //createRoom();
+                        caseSwitch = 0;
+                        break;
+
+                    case 3:
+
+                        break;
+                    //Admin functions
+                    case 10:
+                       
+
+                        break;
+
+                    case 11:
+
+
+                        break;
+                }
+            }
         }
 
         static void readRooms()
@@ -67,5 +107,62 @@ namespace Cinema
             }
             Console.WriteLine("\n");
         }
+
+        static Boolean Login()
+        {
+            while (true) 
+            {
+                
+                Boolean login = false;
+                string username, password = string.Empty;
+                Console.Write("Enter a username: (admin) ");
+                username = Console.ReadLine();
+                if (username == "b") 
+                {
+                    Console.WriteLine("\n\n");
+                    return false;
+                }
+                Console.Write("Enter a password: (admin) ");
+                password = Console.ReadLine();
+
+                if (username == "admin" && password == "admin")
+                {
+                    Console.WriteLine("\n \nWelcome admin");
+                    return true;
+                }
+                else { Console.WriteLine("Wrong input, please try again \n" + "if you want to return to the menu write b as username");}
+
+            }
+        }
+
+        static int Menu(Boolean login) 
+        {
+            int parsable = 0;
+            Console.WriteLine("What action do you want to do?");
+            if (!login) { Console.WriteLine("1:Login \n" + "2:read rooms \n" + "3:function 3 \n" + "4:function 4 \n"); }
+            if (login) { Console.WriteLine("1:Logout \n" + "2:read rooms \n" + "3:function 3 \n" + "4:function 4 \n" + "10:function 1 admin \n" + "11:function 2 admin \n"); }
+            while (true) 
+            {
+                string function = Console.ReadLine();
+                bool isParsable = Int32.TryParse(function, out parsable);
+                if (isParsable)
+                {
+                    if (!login) 
+                    {
+                        if(0 < parsable && parsable < 4) { return parsable; } //number equal to possible functions +1
+                        else { Console.WriteLine("please select only action given");}
+                    }
+                    if (login)
+                    {
+                        if (0 < parsable && parsable < 12) { return parsable; } //number equal to possible functions +1
+                        else { Console.WriteLine("please select only action given"); }
+                    }
+                    
+                }
+                else { Console.WriteLine("please select only action given"); }
+            }
+        }
+
+
     }
 }
