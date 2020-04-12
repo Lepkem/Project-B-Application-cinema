424c28f16d9f0da23aea00cfa2bcf59f3166cb25
Lepkem <Lepkem@users.noreply.github.com>
2020-04-12 15:07:36 +0200

    added IDBank interface and class

---
+++ Cinema/Cinema/IDBank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cinema;
+
+namespace Cinema
+{
+    interface IIDBank
+        {
+            void generateUniqueNumber();
+            void storeOrder();
+            void searchOrder();
+            void editOrder();
+            void deleteOrder();
+        }
+    }
+    class IDBank: IIDBank
+    {
+        public void generateUniqueNumber()
+        {
+            Guid uniqueNumber = new Guid();
+        }
+
+        public void storeOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void searchOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void editOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
