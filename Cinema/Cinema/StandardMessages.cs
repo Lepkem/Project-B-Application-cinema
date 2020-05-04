7f472b2f6e3e006e5084fd07c66529341d1a0256
Lepkem <Lepkem@users.noreply.github.com>
2020-05-04 14:07:44 +0200

    adds standard message class

---
+++ Cinema/Cinema/StandardMessages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema
+{
+    using Console = System.Console;
+
+    class StandardMessages
+    {
+        public static void EnterNumber()
+        {
+            Console.WriteLine($"Please only enter a number!");
+        }
+
+        public static void GivenOptions()
+        {
+            Console.WriteLine($"Only choose from the given options.");
+        }
+
+        public static void SomethingWW(string optionaladdition="")
+        {
+            Console.WriteLine($"Oops! Something went wrong. {optionaladdition}");
+        }
+
+        public static void PressAnyKey()
+        {
+            Console.WriteLine($"Press any key to continue.");
+        }
+    }
+}
