ea0839bac8c8c839037ff5033bfb4898e6acb7fd
Lepkem <Lepkem@users.noreply.github.com>
2020-05-18 23:34:40 +0200

    created printroom and legend

--- Cinema/Cinema/RoomV2.cs
+++ Cinema/Cinema/RoomV2.cs
@@ -8,22 +8,22 @@
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
 
-    //todo OPEN     Standard message class met enkel static void methodes.
-    //todo OPEN     Het programma kan bij startup lezen of er een .txt-bestand in een
-    //bepaalde map is, en de data in dit bestand omzetten naar een JSON-bestand;
-    //zo kunnen nieuwe zalen worden toegevoegd
-    //todo OPEN     De klant moet meerdere stoelen in een rij kunnen selecteren
-    //todo DONE     Als je een kamer aan maak checkt hij niet meer of je input gelijk is
-    //aan de de lengte van row. dus je kan een 5x5 zaal aanmaken met: 1 1 1 1 1 Wat niet hoort te kunnen.
-    //todo TEST     als je de x,y coordinaat in vul dan crashed hij bij een lege input, ook bij
-    //een input die te groot is.
-    //Hier moet een try chatch of een handler om heen.
-    //todo TEST     Onduidelijkheid over de x en y coordinaten
-    //todo TEST     Foolproof maken van het bestellen van een kaartje
-    //todo TEST     Coördinaat invoer bij het bestellen van een kaartje kan bugy zijn boven een
-    //coördinaat dat 10 is
-    //todo OPEN     Zalen worden opgeslagen in .jsons na het bestellen van een kaartje (willen
-    //we een "weet je zeker dat je deze plekken wilt bestellen?" doen?)
+    //todo DONE/NI    Standard message class met enkel static void methodes.
+    //todo DONE/NI    Het programma kan bij startup lezen of er een .txt-bestand in een => CreateFromLayout
+    //                bepaalde map is, en de data in dit bestand omzetten naar een JSON-bestand
+    //                zo kunnen nieuwe zalen worden toegevoegd
+    //todo DONE/NI    De klant moet meerdere stoelen in een rij kunnen selecteren => RoomV2
+    //todo DONE/NI    Als je een kamer aan maak checkt hij niet meer of je input gelijk is
+    //                aan de de lengte van row. dus je kan een 5x5 zaal aanmaken met: 1 1 1 1 1 Wat niet hoort te kunnen.
+    //todo DONE/NI    als je de x,y coordinaat in vul dan crashed hij bij een lege input, ook bij
+    //                een input die te groot is.
+    //                Hier moet een try chatch of een handler om heen.
+    //todo DONE/NI    Onduidelijkheid over de x en y coordinaten
+    //todo DONE/NI    Foolproof maken van het bestellen van een kaartje
+    //todo DONE/NI    Coördinaat invoer bij het bestellen van een kaartje kan bugy zijn boven een
+    //                coördinaat dat 10 is
+    //todo DONE/NI    Zalen worden opgeslagen in .jsons na het bestellen van een kaartje (willen
+    //                we een "weet je zeker dat je deze plekken wilt bestellen?" doen?)
 
     class RoomV2
     {
@@ -50,6 +50,12 @@
 
         public string Name { get; set; }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mapLayout"></param>
+        /// <param name="name"></param>
         public void CreateFromLayout(string[] mapLayout, string name = null)
         {
             this.Name = name;
@@ -93,6 +99,10 @@
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
         public void SaveRoom(string fileName)
         {
             try
@@ -136,44 +146,79 @@
 
 
 
+        /// <summary>
+        /// PrintAvailableSeats 
+        /// </summary>
+        public void PrintAvailableSeats()
+        {
+            var seatsPerRow = seats.Count(s => s.rowNumber == 0);
+            PrintLegend();
 
-        //public void PrintAvailableSeats()
-        //{
-        //    for (uint rowNumber = 1; rowNumber < AmountOfRows; rowNumber++)
-        //    {
-        //        Console.Write($"Row[{rowNumber:D2}| {seats.Count(s => s.rowNumber == rowNumber)}]:\t");
+            Console.Write("    ");
+            for (int i = 1; i < seatsPerRow+1; i++)
+            {
+                Console.Write($"{i:D2} ");
+            }
+            Console.WriteLine();
+            for (uint rowNumber = 1; rowNumber < AmountOfRows; rowNumber++)
+            {
 
-        //        foreach (SeatV2 seatV2 in seats.Where(s => s.rowNumber == rowNumber))
-        //        {
-        //            //Console.Write($"[{seatV2.priceCategory}]");
-        //            Console.Write($"[{(seatV2.vacant ? "-" : "x")}]");
-        //        }
+                Console.Write($"{rowNumber:D2}  ");
 
-        //        Console.WriteLine("");
+                foreach (SeatV2 seatV2 in seats.Where(s => s.rowNumber == rowNumber))
+                {
+                    //Console.Write($"[{seatV2.priceCategory}]");
+                    //Console.Write($"{(seatV2.vacant ? "O" : "x")}  ");
+                    switch (seatV2.priceCategory)
+                    {
+                        case 1:
+                            WriteInColor(ConsoleColor.Green, "O  ");
+                            break;
+                        case 2:
+                            WriteInColor(ConsoleColor.Cyan, "O  ");
+                            break;
+                        case 3:
+                            WriteInColor(ConsoleColor.Yellow, "O  ");
+                            break;
+                        case 0:
+                            WriteInColor(ConsoleColor.Black, "O  ");
+                            break;
+                        default:
+                            WriteInColor(ConsoleColor.Magenta, "O  ");
+                            break;
+                    }
+                }
+
+                string s;
+                Console.WriteLine($"\t |{seats.Count(s => s.rowNumber == rowNumber)} seats available in row");
+                
+            }
+            Console.Write("\n   ");
+            for (int i = 0; i < seatsPerRow; i++)
+            {
+                Console.Write("---");
+            }
+            Console.Write("<== screen");
+        }
+
+        /// <summary>
+        /// PrintLegend is a helper function and prints the legend suited for the PrintRoom
+        /// </summary>
+        public void PrintLegend()
+        {
+            WriteInColor(ConsoleColor.Green, "Price cat \t$\n");
+            WriteInColor(ConsoleColor.Cyan, "Price cat \t$$\n");
+            WriteInColor(ConsoleColor.Yellow, "Price cat \t$$$\n");
+            WriteInColor(ConsoleColor.Red, "unavailable seat\n\n");
+        }
 
 
-        //    }
-        //}
+        private void WriteInColor(ConsoleColor color, string text)
+        {
+            Console.ForegroundColor = color;
+            Console.Write($"{text}");
+            Console.ResetColor();
+        }
 
-        //public void PrintRoom()
-        //{
-        //    //
-        //    for (uint rowNumber = 1; rowNumber < AmountOfRows; rowNumber++)
-        //    {
-        //        Console.Write($"Row[{rowNumber:D2}| {seats.Count(s => s.rowNumber == rowNumber)}]:\t");
-
-        //        foreach (SeatV2 seatV2 in seats.Where(s => s.rowNumber == rowNumber))
-        //        {
-        //            Console.Write($"[{seatV2.priceCategory}]");
-        //           // Console.Write($"[{(seatV2.vacant ? "-" : "x")}]");
-        //        }
-
-        //        Console.WriteLine("");
-
-
-        //    }
-        //}
-
-        //todo  switch true, 
     }
 }
