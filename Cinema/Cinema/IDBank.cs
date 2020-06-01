e654d329c0ddde7f6866992b015b6cc7d3f6f0ec
Mattias <m.heijboer2@students.uu.nl>
2020-06-01 03:17:24 +0200

    search order
    search order

--- Cinema/Cinema/IDBank.cs
+++ Cinema/Cinema/IDBank.cs
@@ -9,20 +9,20 @@ namespace Cinema
     class IDBank
     {
         //Int is the OrderNumber, SchedulElement is the actual movie moment. seat is the specific seats.
-        static Tuple<int, ScheduleElement, List<Tuple<Seat, Tuple<int, int>>>> selectedOrder;
+        static Tuple<string, ScheduleElement, List<Tuple<Seat, Tuple<int, int>>>> selectedOrder;
         
 
-        public static int generateUniqueNumber()
+        public static string generateUniqueNumber()
         {
             Guid uniqueNumber = Guid.NewGuid();
             Console.WriteLine(uniqueNumber);
-            return -1;
+            return uniqueNumber.ToString();
         }
 
         public static void storeOrder(ScheduleElement se, List<Tuple<Seat, Tuple<int, int>>> ls)
         {
-            int ID = generateUniqueNumber();
-            selectedOrder = new Tuple<int, ScheduleElement, List<Tuple<Seat,Tuple<int,int>>>>(ID,se,ls);
+            string ID = generateUniqueNumber();
+            selectedOrder = new Tuple<string, ScheduleElement, List<Tuple<Seat,Tuple<int,int>>>>(ID,se,ls);
             JObject order = new JObject();
             order["ID"] = ID;
             JObject movie = (JObject)JToken.FromObject(se);
@@ -50,9 +50,23 @@ namespace Cinema
             //LET OP DAT DE COORD IN HET JSON MET -1 te weinig wordt gerekend. DUS DAAR MOET REKENING MEE GEHOUDEN WORDEN.
         }
 
-        public void searchOrder()
+        public static void searchOrder()
         {
-            
+            JArray orders = JArray.Parse(File.ReadAllText(@".\orders.json"));
+            Console.WriteLine("Please enter the ID of the order.");
+            string input = Console.ReadLine();
+            foreach (var o in orders)
+                if (input == o["ID"].ToString())
+                {
+                    //This massive sprawling block of spaghetti monster code is supposed to turn the JObject into a correct order to process, don't ask me how
+                    //I wrote this code at 2 AM, the only thing keeping me awake is 00's punk
+                    List<Tuple<Seat, Tuple<int, int>>> seatlist = new List<Tuple<Seat, Tuple<int, int>>>();
+                    foreach (var s in o["Seats"])
+                        seatlist.Add(new Tuple<Seat, Tuple<int, int>>(s["Seat"].ToObject<Seat>(), new Tuple<int, int>(int.Parse(s["Coords"]["X"].ToString()), int.Parse(s["Coords"]["X"].ToString()))));
+                    JToken jse = o["Movie"];
+                    ScheduleElement se = new ScheduleElement(jse["time"].ToString(), jse["movie"].ToObject<Films>(), Program.rooms[int.Parse(jse["room"].ToString())], jse["date"].ToString());
+                    selectedOrder = new Tuple<string, ScheduleElement, List<Tuple<Seat, Tuple<int, int>>>>(input, se, seatlist);
+                }
         }
 
         public void editOrder()
